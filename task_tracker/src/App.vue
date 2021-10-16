d245ae72abe97e6cb4b6611f416f6f7730cc9580
JKAmadio <71294182+JKAmadio@users.noreply.github.com>
2021-10-16 02:51:58 -0300

    tasks list

--- task_tracker/src/App.vue
+++ task_tracker/src/App.vue
@@ -1,19 +1,47 @@
 <template>
-  <div class="w-3/4 mx-auto my-6 p-6 border border-solid border-black">
-    <div class="flex w-full">
-      <Header/>
-    </div>  
+  <div class="w-3/4 mx-auto my-6 p-6 border border-solid border-black bg-gray-50">
+    <Header/>
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 3:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 5th at 9:30am',
+        reminder: true,
+      },
+    ];
   }
 }
 </script>
---
+++ task_tracker/src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="bg-red-100 py-4 px-6 mb-3 shadow">
+		<h3 class="text-lg">
+			{{task.text}}
+		</h3>
+		<p class="text-md">
+			{{task.day}}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Task',
+	props: {
+		task: {type: Object, required:true}
+	},
+}
+</script>
